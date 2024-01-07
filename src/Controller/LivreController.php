7f1ad44cf3ec8be7b7fa2cb194f7bd7ca0138270
angelique KRAUSE <krause_angelique@hotmail.com>
2024-01-07 14:59:21 +0100

    form and templating

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
+use App\Form\LivreType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,13 +23,66 @@ class LivreController extends AbstractController
     //     ]);
     // }
 
-    // Route HOME qui ICI va lister TOUS les livres
+
+    // Route HOME qui va lister TOUS les livres (la page d'accueil listera les Livres)
+
     #[Route('/', name: 'app_home_listeLivres')]
-    // l'<EntityManagerInterface permet d'accéder au Repository (couche Modèle du MVC et ainsi accède aux requêtes et informations de la DB)
     public function home_liste(EntityManagerInterface $entityManager): Response
     {
+        //Récupération de mon Repository Livre
+        $repository = $entityManager->getRepository(Livre::class);
+
+        // Récupération de tous mes livres via findAll()
+        $livres = $repository->findAll();
+
+        // affichage dans le Twig
         return $this->render('livre/index.html.twig', [
-            'liste_home' => 'LivreController',
+            // 'controller_name' => 'LivreController',
+            'livres' => $livres,
         ]);
     }
+
+    // Création de Détail Livre
+    #[Route('/livre/detail/{id}', name: 'app_detailLivre')]
+    public function detail(Livre $livre, EntityManagerInterface $entityManager): Response
+    {
+        // je prends l'id
+        return $this->render('livre/detail.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+
+
+
+    // Ajouter un LIVRE 
+    // l'EntityManagerInterface permet d'accéder au Repository (couche Modèle du MVC et ainsi accèder aux requêtes et informations de la DB)
+    #[Route('/livre/ajouter', name: 'app_livre_ajouter')]
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'un livre
+        $livre = new Livre();
+        // set de la date d'ajout à la date de création
+        $livre->setDateAjout(new DateTime());
+
+        // Création du formulaire
+        $form = $this->createForm(LivreType::class, $livre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData();
+            // but, the original `$task` variable has also been updated
+            $livre = $form->getData();
+
+            // pour envoyer dans la DB
+            $entityManager->persist($livre);
+
+            $entityManager->flush();
+
+            // redirige vers la liste de Livres
+            return $this->redirectToRoute('app_home_listeLivres');
+        }
+        return $this->render('livre/ajout.html.twig');
+    }
+
+
 }
---
+++ src/Form/LivreType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use App\Entity\Livre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('résumé', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('auteur', TextType::class)
+        
+        
+            ->add('dateParution', DateTimeType::class)
+            ->add('genre', EntityType::class, [
+                'class' => Genre::class,
+                'choice_label' => 'nom',
+            ])
+
+            /* ajout d'un bouton de soumission du formulaire */
+            ->add('submit', SubmitType::class, [
+                'label' => 'enregistrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
