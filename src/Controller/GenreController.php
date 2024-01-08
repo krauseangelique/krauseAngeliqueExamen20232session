df4c5592f285afba2bee0c21f841e5c8d4b8ef9f
angelique KRAUSE <krause_angelique@hotmail.com>
2024-01-08 15:10:02 +0100

    create GenreController

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GenreController extends AbstractController
 {
+    // Route par défaut : elle va lister TOUS les genres de livres proposés
     #[Route('/genre', name: 'app_genre')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupération du Repository Genre
+        $repository = $entityManager->getRepository(Genre::class);
+
+        // Récupération des genres via findAll()
+        $genres = $repository->findAll();
+
+        // affichage dans le Twig
         return $this->render('genre/index.html.twig', [
-            'controller_name' => 'GenreController',
+            'genres' => $genres,
         ]);
     }
 }
